90eb991793f10e6415cbc59eaea99dc638cbcc6b
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-20 08:19:15 +0600

    initial commit

---
+++ recap.js
@@ -0,0 +1,10 @@
+// 1. variable deleration with const and let
+// 2. default parameter,5.spread operator
+function defaultParameter(array=[]) {
+    return Math.max(...array);
+}
+// 3. Template string 
+const names = `name is super khan ${square}`;
+
+// 4.arrow function
+const square = x => x * x;
